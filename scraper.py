1616b9a8f6388b8a875c284065bbc80f437eb680
Arnav Prasad <arnavprasad0501@gmail.com>
2026-09-17 19:48:52 -0400

    add remaining scraper methods

--- scraper.py
+++ scraper.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import time
 from nba_api.stats.static import teams
 from nba_api.stats.endpoints import teamgamelog
@@ -14,15 +15,42 @@ class Scraper:
     Find team1 rolling stats across the last N games
 
     @param N    number of previous games to look at
-    @return Nested DataFrame of stats from last N games
+    @return     Nested DataFrame of stats from last N games
     '''
     def getTeam1Stats(self, N):
+        # Find team1Id
         team = next(team for team in teams.get_teams() if team['full_name'] == self.team1)
         team_id = team['id']
+
+        # Scrape game log for team1, then convert to dataframe
         games = teamgamelog.TeamGameLog(
             team_id=team_id,
             season="2025-26",
             season_type_all_star="Regular Season",
         )
         games_df = games.get_data_frames()[0]
+
+        # Sort the games chronologically then return the first N
+        return games_df.sort_values(by="GAME_DATE", ascending=False).head(N)
+
+    '''
+        Find team2 rolling stats across the last N games
+    
+        @param N    number of previous games to look at
+        @return     Nested DataFrame of stats from last N games
+    '''
+    def getTeam2Stats(self, N):
+        # Find team2Id
+        team = next(team for team in teams.get_teams() if team['full_name'] == self.team2)
+        team_id = team['id']
+
+        # Scrape game log for team2, then convert to dataframe
+        games = teamgamelog.TeamGameLog(
+            team_id=team_id,
+            season="2025-26",
+            season_type_all_star="Regular Season",
+        )
+        games_df = games.get_data_frames()[0]
+
+        # Sort the games chronologically then return the first N
         return games_df.sort_values(by="GAME_DATE", ascending=False).head(N)
