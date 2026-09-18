69375b4fa9fdde339c1572247eca5928aac92798
Arnav Prasad <arnavprasad0501@gmail.com>
2026-09-18 13:32:27 -0400

    added method 'gamesDateRange() to fetch game data for training the model within a date range on the calendar.

--- scraper.py
+++ scraper.py
@@ -31,7 +31,7 @@ class Scraper:
             team_id=team_id,
             season="2025-26",
             season_type_all_star="Regular Season",
-        )
+            )
         games_df = games.get_data_frames()[0]
 
         # Convert to datetime, sort the games chronologically and limit to size N
@@ -48,7 +48,7 @@ class Scraper:
             season_nullable="2025-26",
             season_type_nullable="Regular Season",
             measure_type_player_game_logs_nullable="Advanced"
-        )
+            )
         # Sort the advanced stats, same as above
         advanced_df = advanced_stats.get_data_frames()[0]
         advanced_df["GAME_DATE"] = pd.to_datetime(
@@ -57,6 +57,74 @@ class Scraper:
             )
         advanced_df = advanced_df.sort_values(by="GAME_DATE", ascending=False)
         advanced_df = advanced_df.head(N)
+
+        # Merge the two DataFrames into one
+        metrics_of_interest = [
+                            "NET_RATING", "PACE", "TM_TOV_PCT", "EFG_PCT", 
+                            "TS_PCT", "AST_PCT", "AST_TO", "OREB_PCT", 
+                            "DREB_PCT", "REB_PCT", "PIE"
+                            ]
+        games_df = games_df.merge(
+                        advanced_df.set_index("GAME_ID")[metrics_of_interest],
+                        left_on="Game_ID",
+                        right_index=True,
+                        how="left",
+                        )
+
+        # games_df now holds all the needed data
+        return games_df
+
+    '''
+    Find team rolling stats across a given calendar range. Specifically gets
+    "Team_ID", "Game_ID", "GAME_DATE", "MATCHUP", "WL", "W", "L", "W_PCT",
+    "MIN", "OREB", "DREB", "REB", "AST", "STL", "BLK", "TOV", "PF", "PTS", 
+    "NET_RATING", "PACE", "TM_TOV_PCT", "EFG_PCT", "TS_PCT", "AST_PCT", 
+    "AST_TO", "OREB_PCT", "DREB_PCT", "REB_PCT", and "PIE" between two dates.
+
+    @param start    date to begin looking at games, inclusive
+    @param end      date to end looking at games, inclusive
+    @return         Nested DataFrame of stats from given date range
+    @author         Arnav Prasad
+    @version        Sep 18, 2026
+    '''
+    def gamesDateRange(self, start, end):
+        # Find team Id
+        team = next(team for team in teams.get_teams() if team['full_name'] == self.team)
+        team_id = team['id']
+
+        # Scrape game log for team, then convert to dataframe, add start and end dates
+        games = teamgamelog.TeamGameLog(
+            team_id=team_id,
+            season="2025-26",
+            season_type_all_star="Regular Season",
+            date_from_nullable=start,
+            date_to_nullable=end
+            )
+        games_df = games.get_data_frames()[0]
+
+        # Convert to datetime, sort the games chronologically
+        games_df["GAME_DATE"] = pd.to_datetime(
+            games_df["GAME_DATE"], 
+            format="%b %d, %Y"
+            )
+        games_df = games_df.sort_values(by="GAME_DATE", ascending=False)
+
+        # Add advanced stats like Net Rating
+        advanced_stats = teamgamelogs.TeamGameLogs(
+            team_id_nullable=team_id,
+            season_nullable="2025-26",
+            season_type_nullable="Regular Season",
+            measure_type_player_game_logs_nullable="Advanced",
+            date_from_nullable=start,
+            date_to_nullable=end
+            )
+        # Sort the advanced stats, same as above
+        advanced_df = advanced_stats.get_data_frames()[0]
+        advanced_df["GAME_DATE"] = pd.to_datetime(
+            advanced_df["GAME_DATE"], 
+            format="%Y-%m-%dT%H:%M:%S"
+            )
+        advanced_df = advanced_df.sort_values(by="GAME_DATE", ascending=False)
 
         # Merge the two DataFrames into one
         metrics_of_interest = [
@@ -104,35 +172,7 @@ class Scraper:
         # averages now holds the required mean metrics
         return averages
 
-    def gameRanges(self, start, end):
-        # Find team Id
-        team = next(team for team in teams.get_teams() if team['full_name'] == self.team)
-        team_id = team['id']
-
-        # Scrape game log for team, then convert to dataframe
-        games = leaguedashteamstats.LeagueDashTeamStats(
-            measure_type_detailed_defense="Advanced",
-            season="2025-26",
-            season_type_all_star="Regular Season",
-            date_from_nullable=start,
-            date_to_nullable=end
-        )
-        games_df = games.get_data_frames()[0]
-
-        metrics_of_interest = [
-                            "Team_ID", "Game_ID", "GAME_DATE", 
-                            "MATCHUP", "WL", "W", "L", "W_PCT",
-                            "MIN", "OREB", "DREB", "REB", "AST", 
-                            "STL", "BLK", "TOV", "PF", "PTS", 
-                            "NET_RATING", "PACE", "TM_TOV_PCT",
-                            "EFG_PCT", "TS_PCT", "AST_PCT", "AST_TO",
-                            "OREB_PCT", "DREB_PCT", "REB_PCT", "PIE"
-                            ]
-        # games_df = games_df[metrics_of_interest]
-        print(games_df.columns)
-        return games_df
-
 # Tests--
 team = Scraper("Los Angeles Lakers")
 print(team.lastNGames(5))
-print(team.gameRanges(end="2026-04-12", start="2026-04-05"))
+print(team.gamesDateRange(end="2026-04-12", start="2026-04-05"))
