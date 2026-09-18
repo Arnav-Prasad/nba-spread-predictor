a476d7e4fbe3c3f5d919ebe98bb2479fe8851a87
Arnav Prasad <arnavprasad0501@gmail.com>
2026-09-17 22:33:50 -0400

    fix GAME_DATE conversion and sort order in getTeamStats, merged two fetching methods into one

--- scraper.py
+++ scraper.py
@@ -7,22 +7,23 @@ from nba_api.stats.endpoints import teamgamelog
 class Scraper:
 
     # Intialize Scraper object
-    def __init__(self, team1, team2):
-        self.team1 = team1
-        self.team2 = team2
+    def __init__(self, team):
+        self.team = team
 
     '''
-    Find team1 rolling stats across the last N games
+    Find team rolling stats across the last N games
 
     @param N    number of previous games to look at
     @return     Nested DataFrame of stats from last N games
+    @author     Arnav Prasad
+    @version    Sep 17, 2026
     '''
-    def getTeam1Stats(self, N):
-        # Find team1Id
-        team = next(team for team in teams.get_teams() if team['full_name'] == self.team1)
+    def getTeamStats(self, N):
+        # Find team Id
+        team = next(team for team in teams.get_teams() if team['full_name'] == self.team)
         team_id = team['id']
 
-        # Scrape game log for team1, then convert to dataframe
+        # Scrape game log for team, then convert to dataframe
         games = teamgamelog.TeamGameLog(
             team_id=team_id,
             season="2025-26",
@@ -30,27 +31,14 @@ class Scraper:
         )
         games_df = games.get_data_frames()[0]
 
-        # Sort the games chronologically then return the first N
-        return games_df.sort_values(by="GAME_DATE", ascending=False).head(N)
+        # Convert to datetime, sort the games chronologically then return the first N
+        games_df["GAME_DATE"] = pd.to_datetime(
+            games_df["GAME_DATE"], 
+            format="%b %d, %Y"
+            )
+        games_df = games_df.sort_values(by="GAME_DATE", ascending=False)
+        print(games_df["GAME_DATE"].iloc[0])
+        return games_df.head(N)
 
-    '''
-        Find team2 rolling stats across the last N games
-    
-        @param N    number of previous games to look at
-        @return     Nested DataFrame of stats from last N games
-    '''
-    def getTeam2Stats(self, N):
-        # Find team2Id
-        team = next(team for team in teams.get_teams() if team['full_name'] == self.team2)
-        team_id = team['id']
-
-        # Scrape game log for team2, then convert to dataframe
-        games = teamgamelog.TeamGameLog(
-            team_id=team_id,
-            season="2025-26",
-            season_type_all_star="Regular Season",
-        )
-        games_df = games.get_data_frames()[0]
-
-        # Sort the games chronologically then return the first N
-        return games_df.sort_values(by="GAME_DATE", ascending=False).head(N)
+team = Scraper("Los Angeles Lakers")
+print(team.getTeamStats(5))
